c55eb4bbd8874d2caf7ef0b8f2118b62d99ad6b6
zhoushenglei <1178053198@qq.com>
2021-11-18 22:06:30 +0800

    首次提交

---
+++ atcrowdfunding06-common-reverse/src/main/java/com/atguigu/crowd/entity/Admin.java
@@ -0,0 +1,63 @@
+package com.atguigu.crowd.entity;
+
+public class Admin {
+    private Integer id;
+
+    private String loginAcct;
+
+    private String userPswd;
+
+    private String userName;
+
+    private String emall;
+
+    private String createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLoginAcct() {
+        return loginAcct;
+    }
+
+    public void setLoginAcct(String loginAcct) {
+        this.loginAcct = loginAcct == null ? null : loginAcct.trim();
+    }
+
+    public String getUserPswd() {
+        return userPswd;
+    }
+
+    public void setUserPswd(String userPswd) {
+        this.userPswd = userPswd == null ? null : userPswd.trim();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getEmall() {
+        return emall;
+    }
+
+    public void setEmall(String emall) {
+        this.emall = emall == null ? null : emall.trim();
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime == null ? null : createTime.trim();
+    }
+}
